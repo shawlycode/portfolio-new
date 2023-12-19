4ae9f932f7923fd36c8cd603b5eb408a101aedf2
Shawlycode <kutinaorlando@gmail.com>
2023-12-19 15:15:19 +0000

    created sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import Navbar from './section/nav/Navbar'
+import Header from './section/header/Header'
+import About from "./section/about/About"
+import Experience from "./section/experience/Experience"
+import Achievements from './section/achievement/Achievements'
+import Portfolio from './section/portfolio/Portfolio'
+import Testimonials from './section/testimonials/Testimonials'
+import Contact from './section/contact/Contact'
+import Footer from './section/'
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>PORTFOLIO</h1>
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+      <Experience />
+      <Achievements />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/section/nav/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css"
+
+const Nav = () => {
+  return (
+    <div className='nav__bar'>
+      <h2>Navbar</h2>
+    </div>
+  )
+}
+
+export default Nav
