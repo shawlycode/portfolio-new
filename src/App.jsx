4475873fa0f2aacb78c468084227d2a93826239e
Shawlycode <kutinaorlando@gmail.com>
2023-12-19 16:14:16 +0000

    rendered sections in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
+import './app.css'
 import Navbar from './section/nav/Navbar'
+import Projects from './section/portfolio/Projects'
 import Header from './section/header/Header'
-import About from "./section/about/About"
-import Experience from "./section/experience/Experience"
+import About from './section/about/About'
+import Experience from './section/experience/Experience'
 import Achievements from './section/achievement/Achievements'
-import Portfolio from './section/portfolio/Portfolio'
 import Testimonials from './section/testimonials/Testimonials'
 import Contact from './section/contact/Contact'
-import Footer from './section/'
+import Footer from './section/footer/Footer'
 
 const App = () => {
   return (
     <div>
+
       <Navbar />
       <Header />
       <About />
       <Experience />
       <Achievements />
-      <Portfolio />
+      <Projects />
       <Testimonials />
       <Contact />
       <Footer />
+
     </div>
   )
 }
